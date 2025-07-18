16017c133d4566d285680cec4737ea2b2a193e72
Nathaniel <Downeyn21@gmail.com>
2025-07-18 13:12:57 -0400

    keys mapped

--- src/Components/Keypad.jsx
+++ src/Components/Keypad.jsx
@@ -1,11 +1,42 @@
+
+
 function Keypad() {
 
-    const keys = []
+    const keys = [
+        {label: '/'},
+        {label: '*'},
+        {label: '<=', colSpan: 2},
+
+        {label: '7'},
+        {label: '8'},
+        {label: '9'},
+        {label: '-'},
+
+        {label: '4'},
+        {label: '5'},
+        {label: '6'},
+        {label: '+'},
+
+        {label: '1'},
+        {label: '2'},
+        {label: '3'},
+        
+        {label: 'enter', rowSpan: 2},
+        {label: '0', colSpan: 2},
+        {label: '.'}
+    ]
+
 
     return ( 
         <>
             <div className="keypadContainer">
-                
+                {keys.map((key, index) => (
+                    <button
+                    key={index}
+                    >
+                        {key.label}
+                    </button>
+                ))}
             </div>
         </>
     );
